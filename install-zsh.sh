6579b5c6912e3a6d1f76f136c8791e8efc666b63
Xharmagne Carandang <pixie@Xharmagnes-MBP.gateway>
2019-05-21 14:06:40 +1000

    Combine scripts

--- install-zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-curl https://raw.githubusercontent.com/xharmagne/setup/master/.dotfiles/zsh/.zshrc > .zshrc
-
-curl -o '~/Library/Fonts/Meslo LG M Regular for Powerline.ttf' https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf?raw=true
--- install.sh
+++ install.sh
@@ -19,4 +19,10 @@ npm install -g react-native-cli
 brew update && brew cask install react-native-debugger
 
 # Zsh
-source $(dirname "$0")/install-zsh.sh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+curl https://raw.githubusercontent.com/xharmagne/setup/master/.dotfiles/zsh/.zshrc > .zshrc
+
+curl -o '~/Library/Fonts/Meslo LG M Regular for Powerline.ttf' https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf?raw=true
