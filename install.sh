143159c8883d44e12673fbfc1b44b1232c2c90c7
Xharmagne Carandang <pixie@Xharmagnes-MBP.gateway>
2019-05-21 10:15:29 +1000

    Update zsh install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 wait
 brew install git
@@ -18,13 +19,4 @@ brew cask install postman
 brew update && brew cask install react-native-debugger
 
 # Zsh
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-mkdir .dotfiles
-mkdir .dotfiles/zsh
-curl -o .dotfiles/zsh/.myzshrc https://raw.githubusercontent.com/xharmagne/setup/.dotfiles/zsh/.myzshrc
-
-sed -i '' 's/ZSH_THEME=/#ZSH_THEME=/g' .zshrc
-echo -e '\nsource ".dotfiles/zsh/.myzshrc"' >> .zshrc
-
+source $(dirname "$0")/install-zsh.sh
